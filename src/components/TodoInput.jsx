f15c0ba6574124941ed4258a23d2a157d4c0c248
Quaad <ajwood1998@hotmail.co.uk>
2024-11-05 22:21:11 +0000

    setting up and restyling css for todolist

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function TodoInput() {
+  return (
+    <header>
+        <input placeholder='Enter ToDo...' />
+        <button>Add</button>
+    </header>
+  )
+}
